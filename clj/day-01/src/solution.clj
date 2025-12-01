c3689ed2741234fd0a0bca60a8f464ea8f4b8378
Robert Todea <robert.todea@gmail.com>
2025-12-01 15:44:29 +0200

    Refactor dial movement logic to use calculate-next-pos and simplify zero hit counting

--- clj/day-01/src/solution.clj
+++ clj/day-01/src/solution.clj
@@ -25,15 +25,10 @@
   (parse-instruction "L68")  ; Should return {:direction \L, :steps 68}
   (parse-instruction "R48")  ; Should return {:direction \R, :steps 48}
   
-  ;; Test dial movement - FIXED VERSION
-  (move-dial 0 \L 5 100)     ; From 0, move left 5: 99,98,97,96,95 - no zeros hit
-  (move-dial 5 \L 10 100)    ; From 5, move left 10: 4,3,2,1,0,99,98,97,96,95 - hit 0 once
-  (move-dial 95 \R 10 100)   ; From 95, move right 10: 96,97,98,99,0,1,2,3,4,5 - hit 0 once
-  
-  ;; Test edge cases
-  (move-dial 0 \L 1 100)     ; From 0, left 1 step: should go to 99, no zeros hit
-  (move-dial 1 \L 1 100)     ; From 1, left 1 step: should go to 0, hit 0 once
-  (move-dial 99 \R 1 100)    ; From 99, right 1 step: should go to 0, hit 0 once
+  ;; Test dial movement
+  (calculate-next-pos 50 \L 68 100)  ; Should be 82
+  (calculate-next-pos 82 \L 30 100)  ; Should be 52
+  (calculate-next-pos 52 \R 48 100)  ; Should be 0
   
   ;; Test with sample instructions
   (count-dial-zero-is-hit test-instructions)
@@ -46,27 +41,22 @@
         steps (Integer/parseInt (subs instruction 1))]
     {:direction direction :steps steps}))
 
-(defn move-dial
-  "Moves the dial position by the given steps in the given direction.
-   Returns [new-position zero-hits] where zero-hits is how many times we passed 0."
+(defn calculate-next-pos
+  "Calculates the new dial position after rotating."
   [current-pos direction steps dial-size]
-  (let [step-val (if (= direction \R) 1 -1)
-        ;; Generate path of all positions visited (excluding start)
-        path (map #(mod (+ current-pos (* % step-val)) dial-size)
-                  (range 1 (inc steps)))
-        zero-hits (count (filter zero? path))]
-    [(last path) zero-hits]))
+  (let [step-val (if (= direction \R) steps (- steps))]
+    (mod (+ current-pos step-val) dial-size)))
 
 (defn count-dial-zero-is-hit
-  "Counts how many times the dial hits zero."
+  "Counts how many times the dial is left pointing at 0 after any rotation."
   [instructions]
   (let [dial-size 100
-        initial-state [0 0]] ;; [current-pos total-hits]
+        initial-pos 50] ;; Starts at 50
     (second (reduce (fn [[pos total-hits] instruction]
                       (let [{:keys [direction steps]} (parse-instruction instruction)
-                            [new-pos new-hits] (move-dial pos direction steps dial-size)]
-                        [new-pos (+ total-hits new-hits)]))
-                    initial-state
+                            new-pos (calculate-next-pos pos direction steps dial-size)]
+                        [new-pos (if (zero? new-pos) (inc total-hits) total-hits)]))
+                    [initial-pos 0]
                     instructions))))
 
 ;; Main solution using input.txt file
